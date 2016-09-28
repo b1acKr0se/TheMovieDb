9147835ead261f7202809d4b8bd72e28a7290515
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-09-28 13:45:23 +0700

    Add sample request using RxJava

--- app/src/main/java/nt/hai/themoviedb/MainActivity.java
+++ app/src/main/java/nt/hai/themoviedb/MainActivity.java
@@ -1,7 +1,13 @@
 package nt.hai.themoviedb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RetrofitClient.getClient().getPopularMovies(BuildConfig.API_KEY)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .filter(response -> response != null)
+                .flatMap(response -> Observable.from(response.getResults()))
+                .filter(movie -> movie.getVoteAverage() > 5)
+                .subscribe(new Subscriber<Movie>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Movie movies) {
+                        Log.i("onNext", movies.toString());
+                    }
+                });
     }
 }
