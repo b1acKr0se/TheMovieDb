fc895736b6824501093836449df3be4a674537fa
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-09-28 13:52:10 +0700

    Unsubscribe subscriber in onDestroy()

--- app/src/main/java/nt/hai/themoviedb/MainActivity.java
+++ app/src/main/java/nt/hai/themoviedb/MainActivity.java
@@ -10,32 +10,42 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private Subscriber<Movie> subscriber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        subscriber = new Subscriber<Movie>() {
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Movie movie) {
+                Log.i("onNext", movie.toString());
+            }
+        };
+
         RetrofitClient.getClient().getPopularMovies(BuildConfig.API_KEY)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .filter(response -> response != null)
                 .flatMap(response -> Observable.from(response.getResults()))
                 .filter(movie -> movie.getVoteAverage() > 5)
-                .subscribe(new Subscriber<Movie>() {
-                    @Override
-                    public void onCompleted() {
+                .subscribe(subscriber);
+    }
 
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(Movie movies) {
-                        Log.i("onNext", movies.toString());
-                    }
-                });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscriber.unsubscribe();
     }
 }
