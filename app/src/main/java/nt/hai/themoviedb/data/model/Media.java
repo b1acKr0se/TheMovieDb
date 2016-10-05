848ef1e3f7f9dbf413e50d794ea826fb97398de6
Nguyễn Thanh Hải <haint.fit12@gmail.com>
2016-10-05 09:13:38 +0700

    Images and videos endpoint

---
+++ app/src/main/java/nt/hai/themoviedb/data/model/Media.java
@@ -0,0 +1,384 @@
+package nt.hai.themoviedb.data.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Media implements Parcelable {
+
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("popularity")
+    @Expose
+    private Float popularity;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("video")
+    @Expose
+    private Boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private Float voteAverage;
+    @SerializedName("vote_count")
+    @Expose
+    private Integer voteCount;
+
+    @SerializedName("media_type")
+    @Expose
+    private String mediaType;
+
+    private int backgroundColor;
+
+    /**
+     *
+     * @return
+     * The adult
+     */
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    /**
+     *
+     * @param adult
+     * The adult
+     */
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    /**
+     *
+     * @return
+     * The backdropPath
+     */
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    /**
+     *
+     * @param backdropPath
+     * The backdrop_path
+     */
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The originalTitle
+     */
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    /**
+     *
+     * @param originalTitle
+     * The original_title
+     */
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    /**
+     *
+     * @return
+     * The overview
+     */
+    public String getOverview() {
+        return overview;
+    }
+
+    /**
+     *
+     * @param overview
+     * The overview
+     */
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    /**
+     *
+     * @return
+     * The releaseDate
+     */
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    /**
+     *
+     * @param releaseDate
+     * The release_date
+     */
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     *
+     * @return
+     * The posterPath
+     */
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    /**
+     *
+     * @param posterPath
+     * The poster_path
+     */
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    /**
+     *
+     * @return
+     * The popularity
+     */
+    public Float getPopularity() {
+        return popularity;
+    }
+
+    /**
+     *
+     * @param popularity
+     * The popularity
+     */
+    public void setPopularity(Float popularity) {
+        this.popularity = popularity;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @return
+     * The video
+     */
+    public Boolean getVideo() {
+        return video;
+    }
+
+    /**
+     *
+     * @param video
+     * The video
+     */
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    /**
+     *
+     * @return
+     * The voteAverage
+     */
+    public Float getVoteAverage() {
+        return voteAverage;
+    }
+
+    /**
+     *
+     * @param voteAverage
+     * The vote_average
+     */
+    public void setVoteAverage(Float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    /**
+     *
+     * @return
+     * The voteCount
+     */
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    /**
+     *
+     * @param voteCount
+     * The vote_count
+     */
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    @Override
+    public String toString() {
+        return "Media{" +
+                "adult=" + adult +
+                ", backdropPath='" + backdropPath + '\'' +
+                ", id=" + id +
+                ", originalTitle='" + originalTitle + '\'' +
+                ", overview='" + overview + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", posterPath='" + posterPath + '\'' +
+                ", popularity=" + popularity +
+                ", title='" + title + '\'' +
+                ", video=" + video +
+                ", voteAverage=" + voteAverage +
+                ", voteCount=" + voteCount +
+                '}';
+    }
+
+    protected Media(Parcel in) {
+        byte adultVal = in.readByte();
+        adult = adultVal == 0x02 ? null : adultVal != 0x00;
+        backdropPath = in.readString();
+        id = in.readByte() == 0x00 ? null : in.readInt();
+        originalTitle = in.readString();
+        overview = in.readString();
+        releaseDate = in.readString();
+        posterPath = in.readString();
+        popularity = in.readByte() == 0x00 ? null : in.readFloat();
+        title = in.readString();
+        byte videoVal = in.readByte();
+        video = videoVal == 0x02 ? null : videoVal != 0x00;
+        voteAverage = in.readByte() == 0x00 ? null : in.readFloat();
+        voteCount = in.readByte() == 0x00 ? null : in.readInt();
+        backgroundColor = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (adult == null) {
+            dest.writeByte((byte) (0x02));
+        } else {
+            dest.writeByte((byte) (adult ? 0x01 : 0x00));
+        }
+        dest.writeString(backdropPath);
+        if (id == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(id);
+        }
+        dest.writeString(originalTitle);
+        dest.writeString(overview);
+        dest.writeString(releaseDate);
+        dest.writeString(posterPath);
+        if (popularity == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeFloat(popularity);
+        }
+        dest.writeString(title);
+        if (video == null) {
+            dest.writeByte((byte) (0x02));
+        } else {
+            dest.writeByte((byte) (video ? 0x01 : 0x00));
+        }
+        if (voteAverage == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeFloat(voteAverage);
+        }
+        if (voteCount == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(voteCount);
+        }
+        dest.writeInt(backgroundColor);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Media> CREATOR = new Parcelable.Creator<Media>() {
+        @Override
+        public Media createFromParcel(Parcel in) {
+            return new Media(in);
+        }
+
+        @Override
+        public Media[] newArray(int size) {
+            return new Media[size];
+        }
+    };
+}
---
+++ app/src/main/java/nt/hai/themoviedb/util/GlideUtil.java
@@ -0,0 +1,19 @@
+package nt.hai.themoviedb.util;
+
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+public class GlideUtil {
+    public static void load(Context context, String url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .dontAnimate()
+                .dontTransform()
+                .into(imageView);
+    }
+}
